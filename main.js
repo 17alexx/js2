d415d21419c38e515c35895674eefdd66a5d0013
17alexx <ulvia91@icloud.com>
2024-10-28 17:39:54 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+// let number = 5;
+// for (let i = 1; i <= number; i++) {
+//     let tree = '*'.repeat(i);
+//     console.log(tree);
+// }
+
+
+
+// let reqem = 5;
+// for (let i = reqem; i >= 1; i--) {
+//     let tree = '*'.repeat(i);
+//     console.log(tree);
+// }
+
+
+
+// let name = prompt("adinizi daxil edin")
+// let surname = prompt("soyadinizi daxil edin")
+// let age = prompt("dogum ilinizi daxil edin")
+// console.log(name + surname + "siz" + age + ' cu ilde anadan olmusunuz');
+
+
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 5 == 0) {
+        console.log(i);
+    }
+}
